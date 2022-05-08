86f532e9555db87b366d95db6d4bec9b6d5c7a62
sashaboi <onkarcool123@gmail.com>
2022-05-08 19:25:00 +0530

    fixed tweet intent button

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -15,7 +15,7 @@ const Modal = () => {
           <h3>Anon says ...</h3>
         </div>
         <div className="modal-content">{modalContent}</div>
-        <h3>I got this anonymous feedback on www.domain.com</h3>
+        <h3>Get your own on https://feedback-anon.netlify.app/</h3>
         <div onClick={() => setShowModal(false)} className="close-btn">
           <AiFillCloseCircle />
         </div>
